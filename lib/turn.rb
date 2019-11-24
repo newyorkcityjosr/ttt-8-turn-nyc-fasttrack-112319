9c9ae35cfe2f699d6f5a1fda7febe6c59e78bf7e
jose tavarez <newyorkcityjose@gmail.com>
2019-11-24 00:58:00 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,15 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+def vaild_move?(board, index)
+  if index.between? (0, 8) 
+    if !position_taken?(board, index)
+return true    
+else
+  return false
+end
+end
